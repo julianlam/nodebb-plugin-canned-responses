1b5127ce4364c6166184195130e763f3fd0b7c48
Julian Lam <julian@nodebb.org>
2017-03-15 15:04:43 -0400

    closes #11

--- static/lib/main.js
+++ static/lib/main.js
@@ -22,44 +22,77 @@ $(document).ready(function() {
 	$(window).on('action:composer.enhanced', function() {
 		require(['composer/formatting', 'composer/controls'], function(formatting, controls) {
 			formatting.addButtonDispatch('canned-responses', function(textarea, selectionStart, selectionEnd) {
-				$.get(RELATIVE_PATH + '/canned-responses').done(function(data) {
-					data.hideControls = true;
-	
-					templates.parse('partials/canned-responses/list', data, function(html) {
-						var insertIntoComposer = function(ev) {
-								var responseText = submitEl.data('text');
-								controls.insertIntoTextarea(textarea, responseText);
-								controls.updateTextareaSelection(textarea, selectionStart, selectionStart + responseText.length);
-							},
-							modal = bootbox.dialog({
-								title: 'Insert Canned Response',
-								message: html,
-								buttons: {
-									insert: {
-										label: 'Insert',
-										className: 'btn-primary',
-										callback: insertIntoComposer
-									}
-								}
-							}),
-							submitEl = modal.find('.btn-primary').attr('disabled', 'disabled');
-	
-						modal.find('.list-group').on('click', '.list-group-item', function() {
-							var responseEl = $(this);
-							responseEl.siblings().removeClass('active');
-							responseEl.addClass('active');
-	
-							submitEl.data('text', ($(this).find('input[type="hidden"]').val()));
-							submitEl.removeAttr('disabled');
-						});
-	
-						// Go back to focusing on the textarea after modal closes (since that action steals focus)
-						modal.on('hidden.bs.modal', function() {
-							$(textarea).focus();
-						});
+				openModal(function () {
+					var submitEl = this.find('.btn-primary').attr('disabled', 'disabled');
+					var responseText = submitEl.data('text');
+					controls.insertIntoTextarea(textarea, responseText);
+					controls.updateTextareaSelection(textarea, selectionStart, selectionStart + responseText.length);
+
+					// Go back to focusing on the textarea after modal closes (since that action steals focus)
+					this.one('hidden.bs.modal', function() {
+						$(textarea).focus();
 					});
-				});
+				})
 			});
 		});
 	});
+
+	$(window).on('action:composer.loaded', function (ev, data) {
+        if ($.Redactor && $.Redactor.opts.plugins.indexOf('canned-responses') === -1) {
+            $.Redactor.opts.plugins.push('canned-responses');
+        }
+    });
+
+	$(window).on('action:redactor.load', function () {
+        $.Redactor.prototype['canned-responses'] = function () {
+            return {
+                init: function () {
+                    var button = this.button.add('canned-responses', 'Add Canned Response');
+                    this.button.setIcon(button, '<i class="fa fa-bullhorn"></i>');
+                    this.button.addCallback(button, this['canned-responses'].onClick);
+                },
+                onClick: function () {
+					var redactor = this;
+					openModal(function () {
+						var submitEl = this.find('.btn-primary').attr('disabled', 'disabled');
+						var responseText = submitEl.data('text');
+
+						redactor.insert.html('<p>' + responseText + '</p>');
+					});
+                }
+            };
+        };
+    });
+
+	function openModal (callback) {
+		$.get(RELATIVE_PATH + '/canned-responses').done(function(data) {
+			data.hideControls = true;
+
+			templates.parse('partials/canned-responses/list', data, function(html) {
+				var modal = bootbox.dialog({
+						title: 'Insert Canned Response',
+						message: html,
+						buttons: {
+							insert: {
+								label: 'Insert',
+								className: 'btn-primary',
+								callback: function () {
+									callback.call(this)
+								}
+							}
+						}
+					}),
+					submitEl = modal.find('.btn-primary').attr('disabled', 'disabled');
+
+				modal.find('.list-group').on('click', '.list-group-item', function() {
+					var responseEl = $(this);
+					responseEl.siblings().removeClass('active');
+					responseEl.addClass('active');
+
+					submitEl.data('text', ($(this).find('input[type="hidden"]').val()));
+					submitEl.removeAttr('disabled');
+				});
+			});
+		});
+	};
 });
